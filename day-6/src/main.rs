6a2ef6e4b487330bf5ddf662368a25eb10dd0aaa
Brian Carrigan <brian@bcarrigan.com>
2019-12-09 10:28:17 -0500

    Finish Day 6

--- day-6/src/main.rs
+++ day-6/src/main.rs
@@ -44,6 +44,31 @@ impl Galaxy {
        
        total_distance
     }
+
+    fn traverse(&self, from_planet: String, to_planet: String) -> usize {
+        let mut visited: Vec<String> = Vec::new();
+        self.traverse_with_list(from_planet, to_planet, 1, &mut visited).unwrap()
+    }
+
+    fn traverse_with_list(&self, from_planet: String, to_planet: String, distance: usize, visited: &mut Vec<String>) -> Option<usize> {
+        visited.push(from_planet.clone());
+
+        for connection in self.connections(from_planet) {
+            if connection == to_planet {
+                return Some(distance - 2);
+            }
+
+            if visited.contains(&connection) {
+                continue;
+            }
+
+            if let Some(d) = self.traverse_with_list(connection, to_planet.clone(), distance + 1, visited) {
+                return Some(d);
+            }
+        }
+
+        None
+    }
     
     fn orbits_by_parent(&self, root: String) -> Vec<Orbit> {
         (&self.orbits)
@@ -52,11 +77,27 @@ impl Galaxy {
             .map(|o| Orbit { parent: o.parent.clone(), child: o.child.clone() })
             .collect()
     }
+
+    fn connections(&self, root: String) -> Vec<String> {
+        let mut connections: Vec<String> = Vec::new();
+
+        for orbit in &self.orbits {
+            if orbit.child == root {
+                connections.push(orbit.parent.clone());
+            }
+
+            if orbit.parent == root {
+                connections.push(orbit.child.clone());
+            }
+        }
+
+        connections
+    }
 }
 
 fn main() {
     let galaxy = Galaxy::new(fs::read_to_string("input.txt").unwrap());
-    println!("Total orbits: {:?}", galaxy.total_orbits(String::from("COM")));
+    println!("Distance between YOU and SAN: {:?}", galaxy.traverse(String::from("YOU"), String::from("SAN")));
 }
 
 #[test]
@@ -64,4 +105,11 @@ fn test_case() {
     let input = fs::read_to_string("test.txt").unwrap();
     let galaxy = Galaxy::new(input);    
     assert_eq!(galaxy.total_orbits(String::from("COM")), 42);
+}
+
+#[test]
+fn test_traverse() {
+    let input = fs::read_to_string("test2.txt").unwrap();
+    let galaxy = Galaxy::new(input);
+    assert_eq!(galaxy.traverse(String::from("YOU"), String::from("SAN")), 4);
 }
