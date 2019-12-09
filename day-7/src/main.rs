a821f583ecc28acc7cb55ddaf367bf86979859a8
Brian Carrigan <brian@bcarrigan.com>
2019-12-09 13:08:16 -0500

    Finish day 7

--- day-7/src/main.rs
+++ day-7/src/main.rs
@@ -14,27 +14,34 @@ impl std::fmt::Debug for Amplifier {
 
 impl Amplifier {
     fn new(program_code: String, phase_setting: i32) -> Self {
-        Amplifier {
-            program: Program::from(program_code),
-            phase_setting
-        }
+        let mut program = Program::from(program_code);
+        program.append_inputs(&mut vec!(phase_setting));
+        Amplifier { program, phase_setting }
     }
 
     fn run(&mut self, input: i32) -> i32 {
-        let inputs: Vec<i32> = vec!(self.phase_setting, input);
-        let outputs = self.program.run(inputs);
+        let mut inputs: Vec<i32> = vec!(input);
+        let outputs = self.program.run(&mut inputs);
         
         *outputs.last().unwrap()
+    }
+
+    fn run_until_event(&mut self, input: i32) -> intcode::ProgramResult {
+        let mut inputs: Vec<i32> = vec!(input);
+        self.program.append_inputs(&mut inputs);
+
+        self.program.run_until_event()     
     }
 }
 
 struct SweepGenerator {
+    possibilities: Vec<i32>,
     index: usize
 }
 
 impl SweepGenerator {
-    fn new() -> Self {
-        SweepGenerator { index: 0 } 
+    fn new(possibilities: Vec<i32>) -> Self {
+        SweepGenerator { index: 0 , possibilities } 
     }
 }
 
@@ -44,7 +51,7 @@ impl Iterator for SweepGenerator {
     fn next(&mut self) -> Option<Self::Item> {
         if self.index == 5 * 4 * 3 * 2 { return None; }
 
-        let mut possible: Vec<i32> = vec!(0,1,2,3,4);
+        let mut possible: Vec<i32> = self.possibilities.clone();
         let mut current: Vec<i32> = Vec::new();
         
         // Save our index and increment it
@@ -70,13 +77,29 @@ struct SweepResult {
 }
 
 impl SweepResult {
-    fn compute(code: String) -> Self {
+    fn run_array(code: String, possiblities: Vec<i32>) -> Self {
         let mut value = -1000;
         let mut phase_settings: Vec<i32> = Vec::new();
-        let mut generator = SweepGenerator::new();
+        let generator = SweepGenerator::new(possiblities);
     
         for settings in generator {
             let result = run_amplifier_array(code.clone(), &settings);
+            if result > value {
+                value = result;
+                phase_settings = settings;
+            }
+        }
+    
+        SweepResult { phase_settings, value }
+    }
+
+    fn run_feedback_array(code: String, possiblities: Vec<i32>) -> Self {
+        let mut value = -1000;
+        let mut phase_settings: Vec<i32> = Vec::new();
+        let generator = SweepGenerator::new(possiblities);
+    
+        for settings in generator {
+            let result = run_with_feedback(code.clone(), &settings);
             if result > value {
                 value = result;
                 phase_settings = settings;
@@ -101,16 +124,33 @@ fn run_amplifier_array(code: String, phase_settings: &Vec<i32>) -> i32 {
     value
 }
 
+fn run_with_feedback(code: String, phase_settings: &Vec<i32>) -> i32 {
+    let mut value = 0;
+    let mut amplifiers: Vec<Amplifier> = phase_settings
+        .into_iter()
+        .map(|ps| Amplifier::new(code.clone(), *ps))
+        .collect();
+
+    loop {
+        for amplifier in &mut amplifiers {
+            value = match amplifier.run_until_event(value) {
+                intcode::ProgramResult::Output(n) => n,
+                intcode::ProgramResult::Complete => return value
+            };
+        }
+    }
+}
+
 fn main() {
     let code = fs::read_to_string("input.txt").unwrap();
-    println!("{:?}", SweepResult::compute(code));
+    println!("{:?}", SweepResult::run_feedback_array(code, vec!(5,6,7,8,9)));
 }
 
 #[test]
 fn test_amp_1() {
     let phase_settings: Vec<i32> = vec!(4,3,2,1,0);
     let program = String::from("3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0");
-    let results = SweepResult::compute(program);
+    let results = SweepResult::run_array(program, vec!(0,1,2,3,4));
     assert_eq!(results.value, 43210);
     assert_eq!(results.phase_settings, phase_settings);
 }
@@ -119,7 +159,7 @@ fn test_amp_1() {
 fn test_amp_2() {
     let phase_settings: Vec<i32> = vec!(0,1,2,3,4);
     let program = String::from("3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0");
-    let results = SweepResult::compute(program);
+    let results = SweepResult::run_array(program, vec!(0,1,2,3,4));
     assert_eq!(results.value, 54321);
     assert_eq!(results.phase_settings, phase_settings);
 }
@@ -128,7 +168,25 @@ fn test_amp_2() {
 fn test_amp_3() {
     let phase_settings: Vec<i32> = vec!(1,0,4,3,2);
     let program = String::from("3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0");
-    let results = SweepResult::compute(program);
+    let results = SweepResult::run_array(program, vec!(0,1,2,3,4));
     assert_eq!(results.value, 65210);
+    assert_eq!(results.phase_settings, phase_settings);
+}
+
+#[test]
+fn test_amp_feedback_1() {
+    let phase_settings: Vec<i32> = vec!(9,8,7,6,5);
+    let program = String::from("3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5");
+    let results = SweepResult::run_feedback_array(program, vec!(5,6,7,8,9));
+    assert_eq!(results.value, 139629729);
+    assert_eq!(results.phase_settings, phase_settings);
+}
+
+#[test]
+fn test_amp_feedback_2() {
+    let phase_settings: Vec<i32> = vec!(9,7,8,5,6);
+    let program = String::from("3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10");
+    let results = SweepResult::run_feedback_array(program, vec!(5,6,7,8,9));
+    assert_eq!(results.value, 18216);
     assert_eq!(results.phase_settings, phase_settings);
 }
--- intcode/src/lib.rs
+++ intcode/src/lib.rs
@@ -92,108 +92,139 @@ impl ParameterExtension {
 }
 
 pub struct Program {
-    memory: Vec<i32>
+    memory: Vec<i32>,
+    pc: usize,
+    inputs: Vec<i32>
 }
 
 impl From<String> for Program {
     fn from(program_string: String) -> Self {
         Program {
-            memory: program_string.split(",").map(|s| s.parse::<i32>().unwrap()).collect()
+            memory: program_string.split(",").map(|s| s.parse::<i32>().unwrap()).collect(),
+            pc: 0,
+            inputs: Vec::new()
         }
     }
 }
 
+pub enum ProgramResult {
+    Output(i32),
+    Complete
+}
+
 impl Program {
-    pub fn run(&mut self, inputs: Vec<i32>) -> Vec<i32> {
-        let mut pc = 0;
-        let mut input_index = 0;
+    pub fn run(&mut self, inputs: &mut Vec<i32>) -> Vec<i32> {
         let mut codes: Vec<i32> = Vec::new();
 
-        'program: loop {
-            let instruction = Instruction::from(*self.memory.get(pc).unwrap());
+        self.append_inputs(inputs);
 
-            pc = match instruction.code {
+        loop {
+            let code = match self.run_until_event() {
+                ProgramResult::Output(result) => result,
+                ProgramResult::Complete => break
+            };
+
+            codes.push(code);
+        }
+
+        codes
+    }
+
+    pub fn append_inputs(&mut self, inputs: &mut Vec<i32>) {
+        self.inputs.append(inputs);
+    }
+
+    pub fn run_until_event(&mut self) -> ProgramResult {
+        loop {
+            let instruction = Instruction::from(*self.memory.get(self.pc).unwrap());
+            let mut result: Option<ProgramResult> = None;
+
+            self.pc = match instruction.code {
                 InstructionCode::Addition => {
-                    let p1 = self.parameter_for(pc, 1, &instruction.modes);
-                    let p2 = self.parameter_for(pc, 2, &instruction.modes);
+                    let p1 = self.parameter_for(self.pc, 1, &instruction.modes);
+                    let p2 = self.parameter_for(self.pc, 2, &instruction.modes);
 
-                    self.store_result(p1 + p2, self.memory[pc + 3]);
+                    self.store_result(p1 + p2, self.memory[self.pc + 3]);
 
-                    pc + 4
+                    self.pc + 4
                 },
 
                 InstructionCode::Multiplication => {
-                    let p1 = self.parameter_for(pc, 1, &instruction.modes);
-                    let p2 = self.parameter_for(pc, 2, &instruction.modes);
-                    let p3 = self.memory[pc + 3];
+                    let p1 = self.parameter_for(self.pc, 1, &instruction.modes);
+                    let p2 = self.parameter_for(self.pc, 2, &instruction.modes);
+                    let p3 = self.memory[self.pc + 3];
 
                     self.store_result(p1 * p2, p3);
 
-                    pc + 4
+                    self.pc + 4
                 },
 
                 InstructionCode::Input => {
-                    self.store_result(inputs[input_index], self.memory[pc + 1]);
-                    input_index += 1;
+                    let next_input = self.inputs.remove(0);
+                    self.store_result(next_input, self.memory[self.pc + 1]);
 
-                    pc + 2
+                    self.pc + 2
                 },
 
                 InstructionCode::Output => {
-                    codes.push(self.parameter_for(pc, 1, &instruction.modes));
+                    result = Some(ProgramResult::Output(self.parameter_for(self.pc, 1, &instruction.modes)));
 
-                    pc + 2
+                    self.pc + 2
                 },
 
                 InstructionCode::JumpIfTrue => {
-                    let p1 = self.parameter_for(pc, 1, &instruction.modes);
-                    let p2 = self.parameter_for(pc, 2, &instruction.modes);
+                    let p1 = self.parameter_for(self.pc, 1, &instruction.modes);
+                    let p2 = self.parameter_for(self.pc, 2, &instruction.modes);
 
                     match p1 {
-                        0 => pc + 3,
+                        0 => self.pc + 3,
                         _ => p2 as usize
                     }
                 }
 
                 InstructionCode::JumpIfFalse => {
-                    let p1 = self.parameter_for(pc, 1, &instruction.modes);
-                    let p2 = self.parameter_for(pc, 2, &instruction.modes);
+                    let p1 = self.parameter_for(self.pc, 1, &instruction.modes);
+                    let p2 = self.parameter_for(self.pc, 2, &instruction.modes);
 
                     match p1 {
                         0 => p2 as usize,
-                        _ => pc + 3
+                        _ => self.pc + 3
                     }
                 }
 
                 InstructionCode::LessThan => {
-                    let p1 = self.parameter_for(pc, 1, &instruction.modes);
-                    let p2 = self.parameter_for(pc, 2, &instruction.modes);
-                    let p3 = self.memory[pc + 3];
+                    let p1 = self.parameter_for(self.pc, 1, &instruction.modes);
+                    let p2 = self.parameter_for(self.pc, 2, &instruction.modes);
+                    let p3 = self.memory[self.pc + 3];
                     
                     let result = if p1 < p2 { 1 } else { 0 }; 
                     self.store_result(result, p3);
 
-                    pc + 4
+                    self.pc + 4
                 }
 
                 InstructionCode::Equals => {
-                    let p1 = self.parameter_for(pc, 1, &instruction.modes);
-                    let p2 = self.parameter_for(pc, 2, &instruction.modes);
-                    let p3 = self.memory[pc + 3];
+                    let p1 = self.parameter_for(self.pc, 1, &instruction.modes);
+                    let p2 = self.parameter_for(self.pc, 2, &instruction.modes);
+                    let p3 = self.memory[self.pc + 3];
                     
                     let result = if p1 == p2 { 1 } else { 0 }; 
                     self.store_result(result, p3);
 
-                    pc + 4
+                    self.pc + 4
                 }
 
                 InstructionCode::Finish => {
-                    break 'program
+                    result = Some(ProgramResult::Complete);
+
+                    self.pc
                 }
+            };
+
+            if let Some(event) = result {
+                return event;
             }
         }
-
-        codes
     }
 
     fn parameter_for(&self, pc: usize, index: usize, ext: &ParameterExtension) -> i32 {
@@ -213,9 +244,11 @@ impl Program {
 
 #[test]
 fn test_program() {
+    use std::fs;
+
     let input = fs::read_to_string("test.txt").unwrap();
     let mut program = Program::from(input);
-    let diagnostic_codes = program.run(vec!(8));
+    let diagnostic_codes = program.run(&mut vec!(8));
 
     assert_eq!(*diagnostic_codes.last().unwrap(), 1000);
 }
